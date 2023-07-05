b94ecb7e1c6cd4b090a5d8cfb955feeb8b3d832a
duynq <duynq@omt.vn>
2023-07-05 23:02:41 +0700

    route children

---
+++ resources/js/Layouts/AdminLayout.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>Admin layout</h1>
+    <router-view></router-view>
+</template>
+
+<script setup>
+</script>
--- resources/js/Routes/index.js
+++ resources/js/Routes/index.js
@@ -2,26 +2,32 @@ import {createRouter, createWebHistory} from "vue-router";
 import HomePage from "@/Pages/HomePage.vue";
 import AboutPage from "@/Pages/AboutPage.vue";
 import Test from "@/Pages/Test.vue";
+import AdminLayout from "@/Layouts/AdminLayout.vue";
 
 const routes = [
     {
-        path: '/',
-        component: HomePage
-    },
-    {
-        path: '/about',
-        component: AboutPage
-    },
-    {
-        path: '/test',
-        component: Test
+        path: '/admin',
+        component: AdminLayout,
+        children: [
+            {
+                path: '',
+                component: HomePage
+            },
+            {
+                path: 'test',
+                component: Test,
+            },
+            {
+                path: 'about',
+                component: AboutPage,
+            },
+        ],
     },
     {
         path: '/:pathMatch(.*)*',
         name: 'NotFound',
         component: () => import('@/Components/NotFound.vue')
     },
-
 ]
 
 const router = createRouter({
