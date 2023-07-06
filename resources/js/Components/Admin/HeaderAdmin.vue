1a38ec882e753a03b87bd2bd432e6b411bcb7493
DuyNQ <duynq@omt.vn>
2023-07-06 17:57:47 +0700

    create component HeaderAdmin

--- resources/js/Components/Admin/HeaderAdmin.vue
+++ resources/js/Components/Admin/HeaderAdmin.vue
@@ -1,1 +1,17 @@
-<template></template>
+<template>
+    <header class="header">
+
+    </header>
+</template>
+<script setup>
+</script>
+
+<style scoped>
+.header {
+    height: 58px;
+    background-color: #ffffff;
+    z-index: 500;
+    border-bottom: none;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, .05);
+}
+</style>
--- resources/js/Layouts/AdminLayout.vue
+++ resources/js/Layouts/AdminLayout.vue
@@ -1,13 +1,20 @@
 <template>
     <div class="w-full">
-        <header class="h-16 bg-blue-400" style="background-color: #ffffff; z-index: 500;
-                    border-bottom: none;
-                    box-shadow: 0 2px 4px 0 rgba(0,0,0,.05);">
-
-        </header>
+        <HeaderAdmin></HeaderAdmin>
     </div>
     <router-view></router-view>
 </template>
 
 <script setup>
+
+import { defineComponent } from 'vue';
+import HeaderAdmin from '../Components/Admin/HeaderAdmin.vue';
+
+defineComponent({
+    components: {
+        HeaderAdmin
+    }
+})
+
+
 </script>
